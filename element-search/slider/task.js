9420611354fc81668ecb1b55b646eff52c9ec300
Dobro <dobro_88_88@mail.ru>
2023-03-06 13:08:45 +0300

    глобальная переменная(позиция активного слайда реализована локально)

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,7 +2,7 @@ let sliderArrowPrev = Array.from(document.getElementsByClassName("slider__arrow 
 let sliderArrowNext = Array.from(document.getElementsByClassName("slider__arrow slider__arrow_next"))[0];//->
 let sliderDots = Array.from(document.getElementsByClassName("slider__dots")[0].children);// получаем точки.
 let sliderItems = Array.from(document.getElementsByClassName("slider__items")[0].children); //изображения
-let currentNumberImg = 0;
+
 
 function changeImg(newNumberImg)  {
     if( newNumberImg < 0){
@@ -11,6 +11,7 @@ function changeImg(newNumberImg)  {
     else if(newNumberImg >= sliderItems.length){
         newNumberImg = 0;
     }
+    let currentNumberImg = sliderItems.findIndex((element)=>{ return element.className === "slider__item slider__item_active"}) //позиция текущего изображения 
     sliderItems[currentNumberImg].className = "slider__item";
     sliderItems[newNumberImg].className = "slider__item slider__item_active";
     sliderDots[currentNumberImg].className = "slider__dot";
@@ -19,21 +20,18 @@ function changeImg(newNumberImg)  {
 }
 
 sliderArrowNext.onclick = function () {
+    let currentNumberImg = sliderItems.findIndex(element=> { return element.className === "slider__item slider__item_active"})
     changeImg(currentNumberImg + 1);
 }
 
 sliderArrowPrev.onclick = function () {
+    let currentNumberImg = sliderItems.findIndex((element)=>{return element.className === "slider__item slider__item_active"})
     changeImg(currentNumberImg - 1);
 }
 
 sliderDots.forEach((e) => {
     e.onclick = function (even) {
-        let index = 0;
-        for(let i = 0; i< sliderDots.length; i++) {
-            if(even.target === sliderDots[i]){
-                index = i;
-            }
-        }
+        let index = sliderDots.indexOf(even.target);
         changeImg(index);
     }
 })
