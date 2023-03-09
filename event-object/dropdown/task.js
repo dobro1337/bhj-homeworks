79fb69eaafaac3995cb31acc84955d7f9cb10860
Dobro <dobro_88_88@mail.ru>
2023-03-09 21:52:04 +0300

    first

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,19 @@
+let dropDown =  Array.from(document.querySelectorAll(".dropdown"));
+dropDown.forEach((dropDownElement) =>{ //перебираем все dropDown.
+    let dropDownValue = dropDownElement.querySelectorAll(".dropdown__value")[0];
+    let dropDownList = dropDownElement.querySelectorAll(".dropdown__list")[0];
+    let dropDownLink = Array.from(dropDownList.querySelectorAll(".dropdown__link"));
+    
+    dropDownElement.onclick = function () {//записываем функцию вкл и выкл dropDown
+        dropDownList.className = (dropDownList.className === "dropdown__list dropdown__list_active") ?  "dropdown__list" :  "dropdown__list dropdown__list_active";
+    }
+
+    dropDownLink.forEach((element) => {
+        element.onclick = function () {
+            dropDownValue.textContent = this.textContent;
+            return false;
+        }
+    })
+})
+
+
