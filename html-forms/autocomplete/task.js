19894e18538c20143520d6921adf7b86c4744d81
Dobro <dobro_88_88@mail.ru>
2023-03-12 11:17:52 +0300

    second commit

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,6 +68,14 @@ class Autocomplete {
   }
 
   getMatches( text ) {
+    let words = Array.from(this.input.children);
+    let returnWords = [];
+    for(let i =0;i<words.length;i++) {
+      let foundWord = words[i].textContent.includes(text);
+      if(foundWord) {
+        returnWords.push({text: words[i].textContent,value:words[i].value});
+      }
+    }
     /*
       TODO: этот метод нужно дописать
       text - фраза, которую вводят в поле поиска
@@ -81,12 +89,7 @@ class Autocomplete {
         value: 'Содержимое атрибута value'
       }
     */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    return returnWords;
   }
 }
 
